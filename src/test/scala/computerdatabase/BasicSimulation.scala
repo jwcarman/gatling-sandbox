15122b7a5db22bd2b493d343aaf50288ad112e12
James Carman <jcarman@callibrity.com>
2020-05-29 13:32:14 -0400

    Switching to K8S

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -1,19 +1,21 @@
 package computerdatabase
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http
+  val httpProtocol: HttpProtocolBuilder = http
     //.baseUrl("http://localhost:8080")
-    //.baseUrl("https://192.168.50.171")
-    .baseUrl("http://192.168.50.171:18080")
+    .baseUrl("https://192.168.50.171")
+    //.baseUrl("http://192.168.50.171:18080")
     .acceptHeader("text/plain") // Here are the common headers
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+  val scn: ScenarioBuilder = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .exec(http("hello")
       .get("/hello/Foo"))
 
